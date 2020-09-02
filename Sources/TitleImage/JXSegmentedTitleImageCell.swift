5d6833dc120e1b529cda94c29f64c17916aaf480
RipperLiew <824727772@qq.com>
2020-09-02 10:59:47 +0800

    Update JXSegmentedTitleImageCell.swift
    Pinso项目CellUI修改

--- Sources/TitleImage/JXSegmentedTitleImageCell.swift
+++ Sources/TitleImage/JXSegmentedTitleImageCell.swift
@@ -77,6 +77,9 @@ open class JXSegmentedTitleImageCell: JXSegmentedTitleCell {
         var normalImageInfo = myItemModel.normalImageInfo
         if myItemModel.isSelected && myItemModel.selectedImageInfo != nil {
             normalImageInfo = myItemModel.selectedImageInfo
+            backgroundColor = myItemModel.selectedBackgroundColor
+        } else {
+            backgroundColor = myItemModel.normalBackgroundColor
         }
 
         //因为`func reloadData(itemModel: JXSegmentedBaseItemModel, selectedType: JXSegmentedViewItemSelectedType)`方法会回调多次，尤其是左右滚动的时候会调用无数次。如果每次都触发图片加载，会非常消耗性能。所以只会在图片发生了变化的时候，才进行图片加载。
